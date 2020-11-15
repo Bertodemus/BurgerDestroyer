68f757b6801f79f0fc7d33cb6e22696abca5a9bf
Bertodemus <roberto@robertorubet.com>
2020-11-15 12:55:52 -0500

    updated background image and began stylizing the page

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -27,7 +27,8 @@ $(function() {
 
     var newBurger = {
       burger_name: $("#ca").val().trim(),
-      devoured: $("[name=devoured]:checked").val().trim()
+      // devoured: $("[name=devoured]:checked").val().trim()
+      devoured: 0
     };
 
     // Send the POST request.
